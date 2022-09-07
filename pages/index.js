700dd9969adcd5b09041ce2ec942da9259d03e81
carlosalfarodev <rgkalin@gmail.com>
2022-09-06 20:55:47 -0700

    Adds exclamation mark

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to mimizZz</h1>
+        <h1 className={styles.title}>Welcome to mimizZz!</h1>
       </main>
 
       <footer className={styles.footer}>
